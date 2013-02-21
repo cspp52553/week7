59f6bbdb979cebeacf39a7ea749ff07e79ac49fe
Jeff Cohen <jeff@codeacademy.org>
2013-02-20 18:54:45 -0600

    Completed user story #1

--- code/air/app/controllers/flights_controller.rb
+++ code/air/app/controllers/flights_controller.rb
@@ -1,4 +1,6 @@
 class FlightsController < ApplicationController
 
-
+  def index
+    @flights = Flight.order("number asc").limit(500)
+  end
 end
--- code/air/app/models/flight.rb
+++ code/air/app/models/flight.rb
@@ -1,3 +1,28 @@
 class Flight < ActiveRecord::Base
   attr_accessible :arrival_airport, :departs_at, :departure_airport, :number, :seats
+
+  MILEAGE_CHART = {
+    "ORD-JFK" => 740,
+    "ORD-LAX" => 1744,
+    "ORD-SFO" => 1846,
+    "JFK-LAX" => 2475,
+    "JFK-SFO" => 2586,
+    "LAX-SFO" => 338
+  }
+
+  def miles
+    distance = MILEAGE_CHART["#{departure_airport}-#{arrival_airport}"]
+    if distance.nil?
+      distance = MILEAGE_CHART["#{arrival_airport}-#{departure_airport}"]
+    end
+    distance
+  end
+
+  def arrives_at
+    self.departs_at + duration.minutes
+  end
+
+  def duration
+    self.miles / 8
+  end
 end
