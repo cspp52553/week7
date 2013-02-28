ef6242c66eee73872c9251324123493f89a5458f
Jeff Cohen <jeff@codeacademy.org>
2013-02-27 18:03:40 -0600

    Add pagination

--- code/air/app/controllers/flights_controller.rb
+++ code/air/app/controllers/flights_controller.rb
@@ -1,6 +1,6 @@
 class FlightsController < ApplicationController
 
   def index
-    @flights = Flight.order("number asc").limit(500)
+    @flights = Flight.order("number asc").page(params[:page]).per(10)
   end
 end
